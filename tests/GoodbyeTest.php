6ef6b34f49ca99f6fa5e23f7f6a24c0a37600570
Chris Galli <mcgalli@yahoo.com>
2016-05-10 16:16:44 -0600

    Fixing first test, adding another failure with a custom message.

--- tests/GoodbyeTest.php
+++ tests/GoodbyeTest.php
@@ -2,6 +2,10 @@
 
 class GoodbyeTest extends PHPUnit_Framework_TestCase {
   public function testTrue () {
-    $this->assertTrue(false);
+    $this->assertTrue(true);
+  }
+
+  public function testUno () {
+    $this->assertEquals(1,2,"Does this produce an error message in Bamboo?");
   }
 }
